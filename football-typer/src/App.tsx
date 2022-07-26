1c75092759f10dc5da5643997b932cd90cb71c76
GacinhoV33 <fgacek@student.agh.edu.pl>
2022-07-26 12:07:01 +0200

    Base TypeScript template

---
+++ football-typer/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {DragDropContext} from 'react-beautiful-dnd';
+
+function App() {
+  return (
+    <div className="App">
+        lol
+    </div>
+  );
+}
+
+export default App;
