fc89a96b7dde2dbafe54e6f01e0d8b767e5d5b61
Maveric4 <danielgacek97@gmail.com>
2022-10-15 17:09:06 +0200

    tidy 1

---
+++ backend/FootballTyperAPI/Models/MatchJSON.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTyperAPI.Models
+{
+    public class MatchJSON
+    {
+        public int MatchNumber { get; set; }
+        public int RoundNumber { get; set; }
+        public string DateUtc { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string Group { get; set; }
+        public string HomeTeamScore { get; set; }
+        public string AwayTeamScore { get; set; }
+    }
+}
