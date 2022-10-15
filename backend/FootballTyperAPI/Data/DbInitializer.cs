8405617dd3571bada3533025a7446fbfc85cedd0
Maveric4 <danielgacek97@gmail.com>
2022-10-15 17:11:38 +0200

    tidy 4

---
+++ backend/FootballTyperAPI/Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using FootballTyperAPI.Helpers;
+
+namespace FootballTyperAPI.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(FootballTyperAPIContext context)
+        {
+            context.Database.EnsureCreated();
+            if (!context.Teams.Any())
+                FillTeamsTable(context);
+            if (!context.Matches.Any())
+                FillMatchesTable(context);
+        }
+
+
+        public static void CleanDb(FootballTyperAPIContext context)
+        {
+            context.Database.EnsureCreated();
+
+            context.RemoveRange(context.Matches);
+            context.RemoveRange(context.Teams);
+            //context.RemoveRange(context.Bets);
+            context.SaveChanges();
+        }
+
+        public static void FillMatchesTable(FootballTyperAPIContext context)
+        {
+            var matches = MatchHelper.GetAllMatches(context.Teams.ToList());
+            context.AddRange(matches);
+            context.SaveChanges();
+
+        }
+
+        private static void FillTeamsTable(FootballTyperAPIContext context)
+        {
+            var teams = TeamHelper.GetAllValidTeams();
+            context.AddRange(teams);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ backend/FootballTyperAPI/Program.cs
@@ -0,0 +1,60 @@
+using FootballTyperAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FootballTyperAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FootballTyperAPIContext") ?? throw new InvalidOperationException("Connection string 'FootballTyperAPIContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+SetupDatabase(app);
+
+app.Run();
+
+
+//----------------------------------------------------------------------------------------------//
+//---------------------------------------Local functions---------------------------------------//
+
+void SetupDatabase(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<FootballTyperAPIContext>();
+            //DbInitializer.CleanDb(context);
+            DbInitializer.Initialize(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
