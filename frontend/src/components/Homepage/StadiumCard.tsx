e7a6d7c34e97c5dd79f17d2f922f16bdefc20146
GacinhoV33 <fgacek@student.agh.edu.pl>
2022-11-14 23:19:21 +0100

    update data about stadiums

--- frontend/src/components/Homepage/StadiumCard.tsx
+++ frontend/src/components/Homepage/StadiumCard.tsx
@@ -2,33 +2,36 @@ import React, { useEffect, useState } from 'react';
 import './StadiumCard.scss';
 import Image from 'react-bootstrap/Image';
 import { FaCity } from 'react-icons/fa';
-import { BsPeopleFill } from 'react-icons/bs';
+import { BsPeopleFill, BsFillCalendarCheckFill } from 'react-icons/bs';
+import { RiBuilding3Fill } from 'react-icons/ri'
 export interface StadiumCardProps {
-    photo: string[],
-    stadiumName: string,
-    describtion?: string,
-    stadiumSize?: number,
-    stadiumLocation?: string,
-    index: number,
-    setAutoPlay: React.Dispatch<React.SetStateAction<boolean>>,
+  photo: string[],
+  stadiumName: string,
+  opened: string,
+  architect: string,
+  index: number,
+  setAutoPlay: React.Dispatch<React.SetStateAction<boolean>>,
+  describtion?: string,
+  stadiumSize?: number,
+  stadiumLocation?: string,
 }
 
-const StadiumCard: React.FC<StadiumCardProps> = ({photo, describtion, stadiumSize, stadiumLocation, index, stadiumName, setAutoPlay}) => {
+const StadiumCard: React.FC<StadiumCardProps> = ({ photo, describtion, stadiumSize, stadiumLocation, index, stadiumName, setAutoPlay, architect, opened }) => {
   const [currentPhotoIdx, setCurrentPhotoIdx] = useState<number>(0);
   const [currentPhoto, setCurrentPhoto] = useState<string>(photo[currentPhotoIdx]);
 
-  function handleClick(){
+  function handleClick() {
     setAutoPlay(false);
 
-    if(currentPhotoIdx === photo.length - 1){
+    if (currentPhotoIdx === photo.length - 1) {
       setCurrentPhotoIdx(0);
     }
-    else{
+    else {
       setCurrentPhotoIdx(prev => prev + 1);
     }
-    setTimeout(() => { 
-      setAutoPlay(true)   
-    }, 5000   
+    setTimeout(() => {
+      setAutoPlay(true)
+    }, 5000
     )
   }
 
@@ -36,29 +39,41 @@ const StadiumCard: React.FC<StadiumCardProps> = ({photo, describtion, stadiumSiz
     setCurrentPhoto(photo[currentPhotoIdx])
   }, [currentPhotoIdx])
   return (
-   
-                <div className='stadium-card' >
-                    <div onClick={() => handleClick()} className='image-container'>
-                        <img src={currentPhoto} alt={index.toString()} style={{width: '31vw', height: '20vw'}} />
-                    </div>
-                    <div className='bottom-card'>
-                      <div className='stadium-text-name'>
-                          <h4>{stadiumName}</h4>
-                      </div>
-                      <div className='stadium-text-city'>
-                          <FaCity size={'1.5vw'}/>
-                            {stadiumLocation}
-                      </div>
-                      <div className='stadium-text-size'>
-                          <BsPeopleFill size={'1.5vw'}/>{stadiumSize}
-                      </div>
-                      <div>
 
-                      </div>
-                    </div>
-                    
+    <div className='stadium-card' >
+      <div onClick={() => handleClick()} className='image-container'>
+        <img src={currentPhoto} alt={index.toString()} style={{ width: '31vw', height: '20vw' }} />
+      </div>
+      <div className='bottom-card'>
+        <div className='stadium-text-name'>
+          <h4>{stadiumName}</h4>
+        </div>
+        <div className='info'>
+          <div className='row'>
+            <div className='stadium-text-city'>
+              <FaCity size={'1.5vw'} />
+              {stadiumLocation}
+            </div>
+            <div className='stadium-text-size'>
+              <BsPeopleFill size={'1.5vw'} />{stadiumSize}
+            </div>
+          </div>
+          <div className='row'>
+            <div className='opened'>
+              <BsFillCalendarCheckFill size={'1.5vw'} style={{marginRight: '0.5vw'}}/>
+              {opened}
+            </div>
+            <div className='architect'>
+              <RiBuilding3Fill size={'1.5vw'} style={{marginRight: '0.5vw'}}/>
+              {architect}
+            </div>
+          </div>
+        </div>
 
-                  </div>
+      </div>
+
+
+    </div>
   )
 }
 
