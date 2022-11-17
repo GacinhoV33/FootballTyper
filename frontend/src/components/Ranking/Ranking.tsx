9b6d9c520cec266f8ed7c51e8edccdbf820d0975
GacinhoV33 <fgacek@student.agh.edu.pl>
2022-11-17 18:59:51 +0100

    ranking filter api and logic

--- frontend/src/components/Ranking/Ranking.tsx
+++ frontend/src/components/Ranking/Ranking.tsx
@@ -22,35 +22,86 @@ export type RankingProps = {
 
 const Ranking: React.FC<RankingProps> = ({ allUsers }) => {
 
-  const [filter, setFilter] = useState<RankingFilters>('general');
+  const [filter, setFilter] = useState<RankingFilters>('lastDay');
   const [leagueFilter, setLeagueFilter] = useState<string>('main');
   const [usersToDisplay, setUsersToDisplay] = useState<User[]>(allUsers);
-  const [lastDay, setLastDay] = useState<User[]>([]);
-  const [groupStage, setGroupStage] = useState<User[]>([]);
-  const [knockout, setKnockout] = useState<User[]>([]);
-  const [general, setGeneral] = useState<User[]>([]);
-  const leagueUsers = allUsers.filter((user) => user.leagues?.indexOf(leagueFilter) !== -1); //TODO when Api correct delete question mark because there's always at least one leauge
-  const sortedUsers = leagueUsers?.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+  const [lastDayMain, setLastDayMain] = useState<User[]>([]);
+  const [groupStageMain, setGroupStageMain] = useState<User[]>([]);
+  const [knockoutMain, setKnockoutMain] = useState<User[]>([]);
+  const [generalMain, setGeneralMain] = useState<User[]>([]);
+  const [lastDayClown, setLastDayClown] = useState<User[]>([]);
+  const [groupStageClown, setGroupStageClown] = useState<User[]>([]);
+  const [knockoutClown, setKnockoutClown] = useState<User[]>([]);
+  const [generalClown, setGeneralClown] = useState<User[]>([]);
+  
   useEffect(() => {
-    const userToSet = deepcopy(sortedUsers);
-
-    setUsersToDisplay(userToSet)
+    if(leagueFilter === 'main'){
+      switch(filter){
+        case 'general': 
+          setUsersToDisplay(generalMain);
+          break;
+        case 'lastDay':
+          setUsersToDisplay(lastDayMain);
+          break
+        case 'groupStage':
+          setUsersToDisplay(groupStageMain);
+          break;
+        case 'knockoutStage':
+          setUsersToDisplay(knockoutMain);
+          break;
+      }
+    }else{
+      switch(filter){
+        case 'general': 
+          setUsersToDisplay(generalClown);
+          break;
+        case 'lastDay':
+          setUsersToDisplay(lastDayClown);
+          break
+        case 'groupStage':
+          setUsersToDisplay(groupStageClown);
+          break;
+        case 'knockoutStage':
+          setUsersToDisplay(knockoutClown);
+          break;
+      }
+    }
+    
   }
-    , [leagueFilter, allUsers, filter])
+    , [filter, leagueFilter])
 
   useEffect(() => {
     const fetchData = async () => {
-      const lastDay = await (await fetch(process.env.REACT_APP_API_URL + `api/Score/LastDay/${leagueFilter}`)).json();
-      const groupStage = await (await fetch(process.env.REACT_APP_API_URL + `api/Score/Groupstage/${leagueFilter}`)).json();
-      const knockout = await (await fetch(process.env.REACT_APP_API_URL + `api/Score/Knockout/${leagueFilter}`)).json();
-      const general = await (await fetch(process.env.REACT_APP_API_URL + `api/Score/General/${leagueFilter}`)).json();
-      setLastDay(lastDay);
-      setGroupStage(groupStage);
-      setKnockout(knockout);
-      setGeneral(general);
+      const API_URL = process.env.REACT_APP_IS_IT_PRODUCTION_VERSION === 'true' ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_LOCAL;
+
+      const lastDayMain: User[] = await (await fetch(API_URL+ `api/Score/LastDay/main`)).json();
+      const groupStageMain: User[] = await (await fetch(API_URL + `api/Score/Groupstage/main`)).json();
+      const knockoutMain: User[] = await (await fetch(API_URL + `api/Score/Knockout/main`)).json();
+      const generalMain: User[] = await (await fetch(API_URL + `api/Score/All/main`)).json();
+      const lastDayClown: User[] = await (await fetch(API_URL+ `api/Score/LastDay/clownLeague`)).json();
+      const groupStageClown: User[] = await (await fetch(API_URL + `api/Score/Groupstage/clownLeague`)).json();
+      const knockoutClown: User[] = await (await fetch(API_URL + `api/Score/Knockout/clownLeague`)).json();
+      const generalClown: User[] = await (await fetch(API_URL + `api/Score/All/clownLeague`)).json();
+      lastDayMain.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      groupStageMain.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      knockoutMain.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      generalMain.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      lastDayClown.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      groupStageClown.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      knockoutClown.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      generalClown.sort((user1, user2) => user2.totalPoints - user1.totalPoints ? user2.totalPoints - user1.totalPoints : user2.totalExactScoreBets - user1.totalExactScoreBets);
+      setLastDayMain(lastDayMain);
+      setGroupStageMain(groupStageMain);
+      setKnockoutMain(knockoutMain);
+      setGeneralMain(generalMain);
+      setLastDayClown(lastDayClown);
+      setGroupStageClown(groupStageClown);
+      setKnockoutClown(knockoutClown);
+      setGeneralClown(generalClown);
+      setFilter('general');
     }
-    fetchData()
-  }, [leagueFilter])
+    fetchData();
+  }, [])
   return (
     <div className='ranking-main-body'>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
