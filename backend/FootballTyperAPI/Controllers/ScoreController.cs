cdfe67556753d86ffb3827cd43fb84c7d8fe5cd2
Maveric4 <danielgacek97@gmail.com>
2022-11-18 21:57:08 +0100

    ranking position fix

--- backend/FootballTyperAPI/Controllers/ScoreController.cs
+++ backend/FootballTyperAPI/Controllers/ScoreController.cs
@@ -1,9 +1,11 @@
-﻿using FootballTyperAPI.Common;
+﻿using AutoMapper;
+using FootballTyperAPI.Common;
 using FootballTyperAPI.Data;
 using FootballTyperAPI.Models;
 using FootballTyperAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace FootballTyperAPI.Controllers
 {
@@ -13,12 +15,14 @@ namespace FootballTyperAPI.Controllers
     {
         private readonly FootballTyperAPIContext _context;
         private readonly IUserService _userService;
+        private IMapper _mapper;
 
         public ScoreController(FootballTyperAPIContext context,
-            IUserService userService)
+            IUserService userService, IMapper mapper)
         {
             _context = context;
-            _userService = userService;
+            _userService = userService; 
+            _mapper = mapper;
         }
 
         // GET: api/Score/LastDay/main
@@ -38,10 +42,22 @@ namespace FootballTyperAPI.Controllers
             ScoreHelper.CleanUsersData(users);
             if (!lastDayBets.Any())
             {
+                var typerUsersApiEmpty = MapTyperUserApiToTyperUser(users);
+                var rankingListEmpty = RankingHelper.CreateRanking(typerUsersApiEmpty, startPos: 1);
                 //return NotFound(new { msg = $"No bets in this league: {league}" });
+                foreach (var user in users)
+                {
+                    user.PositionDict = rankingListEmpty.First(x => x.User.Id == user.Id).LeaguePosition;
+                }
                 return Ok(users);
             }
             ScoreHelper.CalculatePointsForEachUser(lastDayBets, users);
+            var typerUsersApi = MapTyperUserApiToTyperUser(users);
+            var rankingList = RankingHelper.CreateRanking(typerUsersApi);
+            foreach (var user in users)
+            {
+                user.PositionDict = rankingList.First(x => x.User.Id == user.Id).LeaguePosition;
+            }
             ScoreHelper.UpdateLastFiveUserBets(lastDayBets, users);
             return Ok(users);
         }
@@ -66,10 +82,23 @@ namespace FootballTyperAPI.Controllers
             ScoreHelper.CleanUsersData(users);
             if (!groupStageBets.Any())
             {
+                var typerUsersApiEmpty = MapTyperUserApiToTyperUser(users);
+                var rankingListEmpty = RankingHelper.CreateRanking(typerUsersApiEmpty, startPos: 1);
+                //return NotFound(new { msg = $"No bets in this league: {league}" });
+                foreach (var user in users)
+                {
+                    user.PositionDict = rankingListEmpty.First(x => x.User.Id == user.Id).LeaguePosition;
+                }
                 //return NotFound(new { msg = $"No bets in this league: {league}" });
                 return Ok(users);
             }
             ScoreHelper.CalculatePointsForEachUser(groupStageBets, users);
+            var typerUsersApi = MapTyperUserApiToTyperUser(users);
+            var rankingList = RankingHelper.CreateRanking(typerUsersApi);
+            foreach (var user in users)
+            {
+                user.PositionDict = rankingList.First(x => x.User.Id == user.Id).LeaguePosition;
+            }
             ScoreHelper.UpdateLastFiveUserBets(groupStageBets, users);
             return Ok(users);
         }
@@ -91,12 +120,42 @@ namespace FootballTyperAPI.Controllers
             ScoreHelper.CleanUsersData(users);
             if (!knockoutBets.Any())
             {
+                var typerUsersApiEmpty = MapTyperUserApiToTyperUser(users);
+                var rankingListEmpty = RankingHelper.CreateRanking(typerUsersApiEmpty, startPos: 1);
                 //return NotFound(new { msg = $"No bets in this league: {league}" });
+                foreach(var user in users)
+                {
+                    user.PositionDict = rankingListEmpty.First(x => x.User.Id == user.Id).LeaguePosition;
+                }
                 return Ok(users);
             }
             ScoreHelper.CalculatePointsForEachUser(knockoutBets, users);
+            var typerUsersApi = MapTyperUserApiToTyperUser(users);
+            var rankingList = RankingHelper.CreateRanking(typerUsersApi);
+            foreach (var user in users)
+            {
+                user.PositionDict = rankingList.First(x => x.User.Id == user.Id).LeaguePosition;
+            }
             ScoreHelper.UpdateLastFiveUserBets(knockoutBets, users);
             return Ok(users);
+        }
+
+        private IEnumerable<TyperUser> MapTyperUserApiToTyperUser(IEnumerable<TyperUserApi> users)
+        {
+            var typerUsers = new List<TyperUser>();
+            foreach(var user in users)
+            {
+                var newTyperUser = new TyperUser();
+                newTyperUser.RankStatus = JsonSerializer.Serialize(user.RankStatusDict);
+                newTyperUser.PositionStr = JsonSerializer.Serialize(user.PositionDict);
+                newTyperUser.LeaguesStr = JsonSerializer.Serialize(user.Leagues);
+
+                var userToMap = _mapper.Map(user, newTyperUser);
+
+                typerUsers.Add(userToMap);
+
+            }
+            return typerUsers;
         }
 
         // GET: api/Score/All/main
