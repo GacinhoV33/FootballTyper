bf79371122a8425932c32a6c6ad6ceac79e31f23
Maveric4 <danielgacek97@gmail.com>
2022-11-19 12:11:02 +0100

    security fixes

--- backend/FootballTyperAPI/Data/FootballTyperAPIContext.cs
+++ backend/FootballTyperAPI/Data/FootballTyperAPIContext.cs
@@ -13,6 +13,7 @@ namespace FootballTyperAPI.Data
         public DbSet<Match> Matches { get; set; } = default!;
         public DbSet<Team> Teams { get; set; } = default!;
         public DbSet<Bet> Bets { get; set; } = default!;
+        public DbSet<TyperUser> TyperUser { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -48,8 +49,5 @@ namespace FootballTyperAPI.Data
         {
             return await Bets.Include("Match").Include("Match.HomeTeam").Include("Match.AwayTeam").ToListAsync();
         }
-
-        public DbSet<FootballTyperAPI.Models.TyperUser> TyperUser { get; set; }
-
     }
 }
