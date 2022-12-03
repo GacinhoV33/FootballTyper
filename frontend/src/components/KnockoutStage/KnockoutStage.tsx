8038ccd3963bb2c0881d0e39102f096d3e9e93d4
GacinhoV33 <fgacek@student.agh.edu.pl>
2022-12-03 21:28:21 +0100

    knock fix

--- frontend/src/components/KnockoutStage/KnockoutStage.tsx
+++ frontend/src/components/KnockoutStage/KnockoutStage.tsx
@@ -96,11 +96,11 @@ const CustomSeed = ({ seed, breakpoint, roundIndex, seedIndex }: IRenderSeedProp
               onClick={() => handleBet()}
               disabled={!isBetAllowed}
             >
-              {isBetExisting || betChange !== 0 ? 'Edit' : 'Bet'}
+              {new Date(seed.groupMatch.date) > new Date() ?  (isBetExisting || betChange !== 0  ? 'Edit' : 'Bet') : null}
             </Button>
               : (isBetExisting && isBetNew ?
-                (isBetNew[0].betResult === 1 && new Date(isBetNew[0].match.date) > new Date() ? <BsCheck size={40} style={{ color: 'lightgreen' }} /> : (
-                  isBetNew[0].betResult === 2 && new Date(isBetNew[0].match.date) > new Date() ? <BiCheckDouble size={40} style={{ color: 'darkgreen' }} />
+                (isBetNew[0].betResult === 1 ? <BsCheck size={40} style={{ color: 'lightgreen' }} /> : (
+                  isBetNew[0].betResult === 2 ? <BiCheckDouble size={40} style={{ color: 'darkgreen' }} />
                     : ( isBetNew[0].betResult === 0 ? 
                       <ImCross size={20} style={{ color: 'red', marginRight: '0.5rem' }} /> : null
                     ))
